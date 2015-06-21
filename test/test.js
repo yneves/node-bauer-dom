b8bc624d3e0eae015d5056d9870e01e3e0032982
Yuri Neves Silveira <yurinevessilveira@gmail.com>
2015-06-21 20:31:30 -0300

    update factory

--- test/test.js
+++ test/test.js
@@ -1,11 +1,8 @@
 // - -------------------------------------------------------------------- - //
 // - libs
 
-var lib = {
-	fs: require("fs"),
-	dom: require("../"),
-};
-
+var fs = require("fs");
+var dom = require("../");
 var assert = require("assert");
 
 // - -------------------------------------------------------------------- - //
@@ -13,62 +10,50 @@ var assert = require("assert");
 
 describe("DOM",function() {
 
-	it("each",function() {
-		var dom = lib.dom("<div class=one><div class=two><div class=three><div></div></div></div></div>");
-		var cls = [];
-		dom.find("div").each(function() {
-			cls.push(this.attr("class"));
-		});
-		assert.deepEqual(cls,["one","two","three",undefined]);
-	});
+  it("each",function() {
+    var elm = dom("<div class=one><div class=two><div class=three><div></div></div></div></div>");
+    var cls = [];
+    elm.find("div").each(function() {
+      cls.push(this.attr("class"));
+    });
+    assert.deepEqual(cls,["one","two","three",undefined]);
+  });
 
-	it("map",function() {
-		var dom = lib.dom("<div class=one><div class=two><div class=three><div></div></div></div></div>");
-		var cls = dom.find("div").map(function() {
-			return this.attr("class");
-		});
-		assert.deepEqual(cls,["one","two","three"]);
-	});
+  it("map",function() {
+    var elm = dom("<div class=one><div class=two><div class=three><div></div></div></div></div>");
+    var cls = elm.find("div").map(function() {
+      return this.attr("class");
+    });
+    assert.deepEqual(cls,["one","two","three"]);
+  });
 
-	it("filter",function() {
-		var dom = lib.dom("<div class=one><div class=two><div class=three><div></div></div></div></div>");
-		var len = dom.find("div").filter(function() {
-			return this.attr("class") == "three";
-		}).length;
-		assert.equal(len,1);
-	});
+  it("filter",function() {
+    var elm = dom("<div class=one><div class=two><div class=three><div></div></div></div></div>");
+    var len = elm.find("div").filter(function() {
+      return this.attr("class") == "three";
+    }).length;
+    assert.equal(len,1);
+  });
 
-	it("scrape",function() {
-		var html = lib.fs.readFileSync(__dirname + "/sample.html");
-		var dom = lib.dom(html);
-		var output = dom.scrape({
-			"head > title": {
-				name: "title",
-				data: "text",
-			},
-			"[id]": {
-				name: "ids",
-				each: true,
-				grep: true,
-				data: {
-					"a[href][title]": {
-						name: "urls",
-						each: true,
-						grep: true,
-						data: [{
-							name: "url",
-							data: "href",
-						},{
-							name: "title",
-							data: "title",
-						}],
-					}
-				},
-			},
-		});
-		var sample = JSON.parse(lib.fs.readFileSync(__dirname + "/sample.json","utf8"));
-		assert.deepEqual(output,sample);
-	});
+  it("scrape",function() {
+    var html = fs.readFileSync(__dirname + "/sample.html");
+    var elm = dom(html);
+    var output = elm.scrape({
+      "head > title": {
+        title: "text",
+      },
+      "[id]": {
+        ids: {
+          "a[href][title]": {
+            url: "attr:href",
+            title: "attr:title"
+          }
+        }
+      }
+    });
+    var sample = JSON.parse(fs.readFileSync(__dirname + "/sample.json","utf8"));
+    assert.deepEqual(output,sample);
+  });
 
 });
 
